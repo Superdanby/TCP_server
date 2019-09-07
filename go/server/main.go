699fb61bf777381d4a824641dcf3da52ee18eecb
Henry Tseng <henrybear327@gmail.com>
2019-09-07 13:18:59 -0700

    update golang server

--- go/server/main.go
+++ go/server/main.go
@@ -1,23 +1,153 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"bytes"
 	"log"
+	"net"
 	"net/http"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	counter uint64
+	active  int64
+)
+
+const (
+	maxConcurrentRequests = 1
 )
 
 func main() {
+	// init
+	counter = 0
+	active = 0
+	maxConcurrencyLimit := make(chan bool, maxConcurrentRequests)
+
 	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100 // connect: can't assign requested address
 	// runtime.GOMAXPROCS(1)
 
-	http.HandleFunc("/", requestHandler)
+	// setup stat thread
+	go stat()
 
-	log.Println("Ready to start")
-	log.Println(http.ListenAndServe(":8888", nil))
+	// setup server
+	server, err := newServer(":8888")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		// TODO: gracefully stop the server
+
+		err := server.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("Server stops")
+	}()
+
+	// start taking connections
+	log.Println("Server starts")
+	for {
+		log.Println("Waiting for chan...")
+		maxConcurrencyLimit <- true
+		log.Println("Waiting for TCP...")
+		conn, err := server.AcceptTCP()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		go handler(conn, maxConcurrencyLimit)
+	}
 }
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	// log.Println("Start")
-	fmt.Fprintf(w, "200 foo\n")
-	// defer log.Println("Done")
+func stat() {
+	for {
+		log.Printf("Request received %d, active %d\n", counter, atomic.LoadInt64(&active))
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func newServer(address string) (server *net.TCPListener, err error) {
+	addr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	tcpListener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return tcpListener, nil
+}
+
+func handler(_conn *net.TCPConn, maxConcurrencyLimit <-chan bool) {
+	atomic.AddInt64(&active, 1)
+	conn := newConnection(_conn)
+	log.Printf("new connection id %d, remote %s, local %s\n", conn.getID(), conn.RemoteAddr(), conn.LocalAddr().String())
+
+	defer func(conn *connection) {
+		id := conn.getID()
+		remoteAddr := conn.RemoteAddr()
+		log.Printf("closing connection %d, %s\n", id, remoteAddr)
+		err := conn.Close()
+		if err != nil {
+			log.Fatalln("closing connection error", err)
+		}
+		log.Printf("connection %d, %s closed\n", id, remoteAddr)
+		log.Println("channel freed", <-maxConcurrencyLimit)
+		atomic.AddInt64(&active, -1)
+	}(conn)
+
+	err := conn.decodeConnection()
+	if err != nil {
+		log.Printf("connection %d, %s has error\n", conn.getID(), err.Error())
+	}
+}
+
+type connection struct {
+	id uint64
+
+	*net.TCPConn // inheritence
+}
+
+func newConnection(c *net.TCPConn) *connection {
+	return &connection{
+		id:      atomic.AddUint64(&counter, 1),
+		TCPConn: c,
+	}
+}
+
+func (c *connection) getID() uint64 {
+	return c.id
+}
+
+func (c *connection) send(code, msg string) (int, error) {
+	var buf = bytes.NewBufferString(code + " " + msg + "\n")
+
+	return c.Write(buf.Bytes())
+}
+
+func (c *connection) decodeConnection() error {
+	scanner := bufio.NewScanner(c.TCPConn)
+
+	// Read from connection with line split
+	for scanner.Scan() {
+		buf := scanner.Bytes()
+		log.Printf("decoded = %s", string(buf))
+
+		// send out reply
+		// n, err := c.send("200", "foo")
+		_, err := c.send("200", "foo")
+		if err != nil {
+			log.Fatalln("send response error", err)
+		}
+		// log.Println("Response count", n)
+	}
+	if scanner.Err() != nil {
+		log.Fatalln("scanner error", scanner.Err())
+	}
+
+	return nil
 }
