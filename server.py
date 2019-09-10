c6cc456a47366574cc7c0b7e75a1034cc3d421a4
Superdanby <dannymrt1@gmail.com>
2019-09-10 16:13:22 +0800

    Remove 1 query per connection mode

--- server.py
+++ server.py
@@ -39,27 +39,6 @@ async def proton_api(message, cmd='printf'):
 
 async def handle_query(reader, writer):
     global cnt
-    data = await reader.read(100000000000)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
-    cmprs = writer.get_extra_info('compression')
-
-    # print(f"Received {message!r} from {addr!r}")
-    print(f"Received {message!r} from {addr!r}, compression: {cmprs}")
-
-    # wait for response from Proton API
-    response = await proton_api(message)
-    # response = "yee".encode()
-
-    writer.write(response)
-    await writer.drain()
-
-    print("Close the connection")
-    writer.close()
-    cnt = cnt + 1
-
-async def handle_query_persistent(reader, writer):
-    global cnt
     while True:
         data = await reader.read(100000000000)
         message = data.decode()
@@ -82,12 +61,8 @@ async def handle_query_persistent(reader, writer):
     print("Close the connection")
     writer.close()
 
-async def main(address='127.0.0.1', port=8888, persistent=False):
-    if persistent:
-        server = await asyncio.start_server(handle_query_persistent, address, port)
-    else:
-        server = await asyncio.start_server(handle_query, address, port)
-
+async def main(address='127.0.0.1', port=8888):
+    server = await asyncio.start_server(handle_query, address, port)
 
     addr = server.sockets[0].getsockname()
     print(f'Serving on {addr}')
@@ -100,10 +75,9 @@ async def main(address='127.0.0.1', port=8888, persistent=False):
 parser = argparse.ArgumentParser(description='Specify server address and port')
 parser.add_argument('address', type=str, nargs=1, help='server address')
 parser.add_argument('port', type=int, nargs=1, help='server port')
-parser.add_argument('--persistent', action='store_true', help='open connection until client sends "close"')
 args = parser.parse_args()
 
 # print(args)
 
 if __name__ == '__main__':
-    asyncio.run(main(address=args.address[0], port=args.port[0], persistent=args.persistent))
+    asyncio.run(main(address=args.address[0], port=args.port[0]))
