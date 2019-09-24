33916ee54dd23bacefb929312cc7b6bf0db9ebf8
Superdanby <dannymrt1@gmail.com>
2019-09-24 19:51:19 +0800

    Add min heap to ensure client FIFO

--- server-aiohttp.py
+++ server-aiohttp.py
@@ -1,47 +1,66 @@
 import asyncio
 import aiohttp # pip install aiohttp[speedups]
 import argparse
-import time
-import cProfile, pstats, io
-from pstats import SortKey
+# import cProfile, pstats, io
+import heapq
+# from pstats import SortKey
 import ssl
+# import time
 
-semaphore = 3e4
+semaphore = 100
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-async def respond(writer, message, api_session):
-    """Wait for the results from proton_api and send it to the client, writes and reads with Proton API is protected with a lock to ensure FIFO order"""
+async def respond(writer, message, api_session, response_queue, receive_cnt, transmit_control):
+    """Wait for the results from proton_api and send it to the client, requests are labelled with receive_cnt and pushed into a min heap to ensure FIFO order"""
     global semaphore
     # writer.write("200 permit\n".encode())
     # await writer.drain()
     # semaphore = semaphore + 1
     # return
 
-    print('https://localhost/api/mail/incoming/recipient?Email=' + message[4:-1])
-    try:
-        resp = await api_session.get('https://localhost/api/mail/incoming/recipient?Email=' + message[4:-1])
-        async with resp:
-            if resp.status == 200 or resp.status == 204:
-                response = "200 permit\n"
-            elif resp.status == 422:
-                dict = await resp.json()
-                response = "200 reject\n"
-            elif resp.status == 400:
-                response = "500 bad request\n"
-            else:
-                response = "400 " + dict['Error'] + "\n"
-    except:
-        response = "500 bad request\n"
+    # print('https://localhost/api/mail/incoming/recipient?Email=' + message[4:-1])
+    while True:
+        try:
+            resp = await api_session.get('https://localhost/api/mail/incoming/recipient?Email=' + message[4:-1])
+            async with resp:
+                # print(resp.status)
+                if resp.status == 200 or resp.status == 204:
+                    response = "200 permit\n"
+                elif resp.status == 422:
+                    dict = await resp.json()
+                    response = "200 reject\n"
+                elif resp.status == 400:
+                    response = "500 bad request\n"
+                else:
+                    response = "400 " + dict['Error'] + "\n"
+            break
+        except:
+            # print("connection failed")
+            pass
 
-    writer.write(response.encode())
-    # prevent the server from outputing too much error caused by connection failure, thus not serving new requests
-    try:
-        await writer.drain()
-    except:
-        pass
+    # insert an element: [incoming order, encoded response]
+    heapq.heappush(response_queue, [receive_cnt, response.encode()])
+    # print(response_queue)
+    transmit_control[0] = transmit_control[0] + 1
+    # print(f"readyt: {response_queue[0]}, expectedt: {transmit_control[1]}")
+
+    # If response_queue is not empty and the element on top of min-heap has the smallest receive_cnt of all elements not yet sent, send the element. Repeat this step until the condition is not met.
+    while transmit_control[0] != 0 and response_queue[0][0] == transmit_control[1]:
+        _, leaving = heapq.heappop(response_queue)
+        # assert(leaving != b'')
+        transmit_control[1] = transmit_control[1] + 1
+        transmit_control[0] = transmit_control[0] - 1
+        # prevent the server from outputing too much error caused by connection failure, thus not serving new requests
+        try:
+            # reply to client
+            writer.write(leaving)
+            # print(f"{_}, {leaving}")
+            await writer.drain()
+        except:
+            pass
     semaphore = semaphore + 1
 
 async def handle_query(reader, writer, api_session):
@@ -49,8 +68,10 @@ async def handle_query(reader, writer, api_session):
     # pr = cProfile.Profile()
     # pr.enable()
     global semaphore
-    cnt = 0
     tasks = []
+    response_queue = [] # min heap
+    transmit_control = [0, 0] # size of response_queue, transmit count
+    receive_cnt = 0 # receive cnt
     while True:
         # read data from client
         data = await reader.readline()
@@ -64,8 +85,9 @@ async def handle_query(reader, writer, api_session):
             # semaphore is initialized with 30k, which takes about 1.5s to process all requests, sleep for 1 second have a smaller impact on performance compared to sleep for 0 seconds
             await asyncio.sleep(1)
         # non blocking function call to respond()
-        tasks.append(asyncio.create_task(respond(writer, message, api_session)))
+        tasks.append(asyncio.create_task(respond(writer, message, api_session, response_queue, receive_cnt, transmit_control)))
         semaphore = semaphore - 1
+        receive_cnt = receive_cnt + 1
 
         # reset connection when EOF reached
         if reader.at_eof():
